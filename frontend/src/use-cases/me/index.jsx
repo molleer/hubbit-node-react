5c5bba97cc891beb07a13cc75449a5ab3e806278
David Möller <davidm@student.chalmers.se>
2021-05-28 10:32:28 +0200

    Impelemented add functionality in backend

--- frontend/src/use-cases/me/index.jsx
+++ frontend/src/use-cases/me/index.jsx
@@ -1,4 +1,8 @@
-import { DigitCRUD, DigitLayout } from "@cthit/react-digit-components";
+import {
+    DigitCRUD,
+    DigitLayout,
+    DigitTextField
+} from "@cthit/react-digit-components";
 import axios from "axios";
 
 const Me = () => {
@@ -12,6 +16,7 @@ const Me = () => {
                 <DigitCRUD
                     path="/me"
                     readAllRequest={() => axios.get("/api/devices")}
+                    createRequest={device => axios.post("/api/devices", device)}
                     keysOrder={["device_name", "address"]}
                     keysText={{
                         device_name: "Device Name",
@@ -23,6 +28,25 @@ const Me = () => {
                         emptyTableText: "You have not added any devices",
                         startOrderBy: "device_name"
                     }}
+                    formComponentData={{
+                        device_name: {
+                            component: DigitTextField,
+                            componentProps: {
+                                outlined: true,
+                                upperLabel: "Device name"
+                            }
+                        },
+                        address: {
+                            component: DigitTextField,
+                            componentProps: {
+                                outlined: true,
+                                upperLabel: "Address",
+                                lowerLabel: "Form: 00:00:00:00:00:00"
+                            }
+                        }
+                    }}
+                    createButtonText="New device"
+                    createTitle="New device"
                 />
             </DigitLayout.Column>
         </DigitLayout.Row>
